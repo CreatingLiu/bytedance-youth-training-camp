aca450308bb56375ece752118e680b8f79ff3fa5
CreatingLiu <773673499@qq.com>
2021-09-20 14:29:25 +0800

    bdyyds

--- src/day1/vue_project/src/App.vue
+++ src/day1/vue_project/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> 
+      | <router-link to="/abc">abc</router-link>
       | <router-link to="/about">about</router-link>
     </div>
     <router-view/>
